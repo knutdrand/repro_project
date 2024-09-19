eae3a7a7d1ae5b2200cb8e7f0b483c3ad83ee87b
Knut Rand <knutdrand@gmail.com>
2024-09-19 10:14:23 +0200

    first test

--- repro_project/repro_project.py
+++ repro_project/repro_project.py
@@ -1,1 +1,6 @@
 """Main module."""
+
+def count_gc(seq):
+    """Count GC content in a sequence."""
+    seq = seq.upper()
+    return (seq.count('G') + seq.count('C')) / len(seq)
--- tests/test_repro_project.py
+++ tests/test_repro_project.py
@@ -4,18 +4,23 @@
 
 import pytest
 
+from repro_project.repro_project import count_gc
+
 
 @pytest.fixture
-def response():
+def seq():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
+    return 'acgttc'
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
+def test_count_gc(seq):
     """Sample pytest test function with the pytest fixture as an argument."""
+    assert count_gc(seq) == 0.5
+
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
